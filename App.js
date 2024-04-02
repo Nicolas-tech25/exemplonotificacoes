ffaf403f80064fa70654e2e17a0def81c247cc01
@Nicolas_Oliveira <125362779+Nicolas-tech25@users.noreply.github.com>
2024-04-02 11:00:18 -0300

    commit

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import { useState, useEffect } from "react";
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+    };
+  },
+});
 
 export default function App() {
+  const [dados, setDados] = useState(null);
+  useEffect(() => {
+    async function permissoesIos() {
+      return await Notifications.requestPermissionsAsync({
+        ios: {
+          allowAlert: true,
+          allowSound: true,
+          allowBadge: true,
+        },
+      });
+    }
+  });
+  const enviarMensagem = () => {};
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar>
+        <View style={styles.container}>
+          <Text>Exemplo de notificação local</Text>
+          <Button
+            title="Disparar notificação"
+            onPress={enviarMensagem}
+          ></Button>
+        </View>
+      </StatusBar>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
